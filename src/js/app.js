dc6cc60a5da7eff1874b26a6a605a4419b1ec890
Vito Senic <67893442+pegi4@users.noreply.github.com>
2024-08-20 08:00:15 +0200

    update

--- src/js/app.js
+++ src/js/app.js
@@ -14,24 +14,22 @@ function switchLang(lang) {
       }
 
       // Update language buttons visibility
-      const langSelect = document.querySelector('.lang-select');
+      const langSelect = document.querySelector(".lang-select");
       const slButton = langSelect.querySelector('button[data-lang="sl"]');
       const enButton = langSelect.querySelector('button[data-lang="en"]');
 
-      if (lang === 'sl') {
-        slButton.style.display = 'none';
-        enButton.style.display = 'inline-block';
-      } else if (lang === 'en') {
-        slButton.style.display = 'inline-block';
-        enButton.style.display = 'none';
+      if (lang === "sl") {
+        slButton.style.display = "none";
+        enButton.style.display = "inline-block";
+      } else if (lang === "en") {
+        slButton.style.display = "inline-block";
+        enButton.style.display = "none";
       }
     })
-    .catch((error) => console.error('Error loading translations:', error));
+    .catch((error) => console.error("Error loading translations:", error));
 }
 
-switchLang('sl');
-
-
+switchLang("sl");
 
 /* FIRST SECTION ARROW CLICK */
 
@@ -40,64 +38,65 @@ function scrollToFirstSection() {
   const firstSection = document.getElementById("section1");
   window.scrollTo({
     top: firstSection.offsetTop,
-    behavior: "smooth"
+    behavior: "smooth",
   });
 }
 
 // Add event listener to the scroll hint
-document.getElementById("scroll-hint").addEventListener("click", scrollToFirstSection);
-
-
+document
+  .getElementById("scroll-hint")
+  .addEventListener("click", scrollToFirstSection);
 
 /* NAVIGATION */
 
-  // Smooth scrolling to the target section
-  function scrollToSection(event) {
-    event.preventDefault();
-    const target = document.querySelector(this.getAttribute("href"));
-    window.scrollTo({
-      top: target.offsetTop,
-      behavior: "smooth"
-    });
-  }
+// Smooth scrolling to the target section
+function scrollToSection(event) {
+  event.preventDefault();
+  const target = document.querySelector(this.getAttribute("href"));
+  window.scrollTo({
+    top: target.offsetTop,
+    behavior: "smooth",
+  });
+}
 
-  document.getElementById("scroll-hint").addEventListener("click", scrollToSection);
+document
+  .getElementById("scroll-hint")
+  .addEventListener("click", scrollToSection);
 
-  // Update the active dot based on the current scroll position
-  function updateActiveDot() {
-    const sections = document.querySelectorAll("section");
-    const dots = document.querySelectorAll(".dot");
+// Update the active dot based on the current scroll position
+function updateActiveDot() {
+  const sections = document.querySelectorAll("section");
+  const dots = document.querySelectorAll(".dot");
 
-    sections.forEach((section, index) => {
-      const sectionTop = section.offsetTop;
-      const sectionHeight = section.clientHeight;
-      if (
-        window.scrollY >= sectionTop &&
-        window.scrollY < sectionTop + sectionHeight
-      ) {
-        dots.forEach(dot => dot.classList.remove("active"));
-        dots[index].classList.add("active");
-      }
-    });
-  }
+  sections.forEach((section, index) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    if (
+      window.scrollY >= sectionTop &&
+      window.scrollY < sectionTop + sectionHeight
+    ) {
+      dots.forEach((dot) => dot.classList.remove("active"));
+      dots[index].classList.add("active");
+    }
+  });
+}
 
-  // Add event listeners to dots and scroll event
-  document.querySelectorAll(".dot").forEach(dot =>
-    dot.addEventListener("click", scrollToSection)
-  );
-  window.addEventListener("scroll", updateActiveDot);
-
+// Add event listeners to dots and scroll event
+document
+  .querySelectorAll(".dot")
+  .forEach((dot) => dot.addEventListener("click", scrollToSection));
+window.addEventListener("scroll", updateActiveDot);
 
 /* ANIMATION */
 
-document.addEventListener('DOMContentLoaded', () => {
-  const typewriterText = document.getElementById('typewriter-text');
-  const heading = document.getElementById('heading');
-  const socialLinks = document.getElementById('social-links');
-  const emailButton = document.getElementById('email-button');
-  const scrollHint = document.getElementById('scroll-hint');
-  const navigation = document.getElementById('navigation');
-  const langSet = document.getElementById('lang-set');
+document.addEventListener("DOMContentLoaded", () => {
+  const typewriterText = document.getElementById("typewriter-text");
+  const heading = document.getElementById("heading");
+  const socialLinks = document.getElementById("social-links");
+  const emailButton = document.getElementById("email-button");
+  const scrollHint = document.getElementById("scroll-hint");
+  const navigation = document.getElementById("navigation");
+  const langSet = document.getElementById("lang-set");
 
   const typewriterTextContent = "Hey, I'm";
   const headingContent = "Vito Senič";
@@ -125,13 +124,13 @@ document.addEventListener('DOMContentLoaded', () => {
     await typeWriterEffect(typewriterText, typewriterTextContent);
     await typeWriterEffect(heading, headingContent);
     setTimeout(() => {
-      socialLinks.classList.add('show');
-      navigation.classList.add('show');
-      langSet.classList.add('show');
+      socialLinks.classList.add("show");
+      navigation.classList.add("show");
+      langSet.classList.add("show");
       setTimeout(() => {
-        emailButton.classList.add('show');
+        emailButton.classList.add("show");
         setTimeout(() => {
-          scrollHint.classList.add('show');
+          scrollHint.classList.add("show");
         }, scrollHintDelay - emailButtonDelay);
       }, emailButtonDelay - socialLinksDelay);
     }, socialLinksDelay);
@@ -139,3 +138,22 @@ document.addEventListener('DOMContentLoaded', () => {
 
   animateHeader();
 });
+
+// Function to calculate age
+function calculateAge() {
+  const birthdate = new Date("2004-09-26");
+  const today = new Date();
+
+  let age = today.getFullYear() - birthdate.getFullYear();
+
+  const monthDifference = today.getMonth() - birthdate.getMonth();
+  const dayDifference = today.getDate() - birthdate.getDate();
+
+  if (monthDifference < 0 || (monthDifference === 0 && dayDifference < 0)) {
+    age--;
+  }
+
+  document.getElementById("ac-age").textContent = age;
+}
+
+window.onload = calculateAge;
